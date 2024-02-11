23234b29629e15e517096349fce0b900c73d477f
peixed351 <peixed351@gmail.com>
2024-02-11 13:14:50 +0300

    add settings

--- ABSTRACT.md
+++ ABSTRACT.md
@@ -1,5 +1,10 @@
+The **Wildtrack: Multi-Camera Person Dataset** brings multi-camera detection and tracking methods into the wild. It meets the need of the deep learning methods for a large-scale multi-camera dataset of walking pedestrians, where the cameras’ fields of view in large part overlap. Being acquired by current high tech hardware it provides HD resolution data. Further, its high precision joint calibration and synchronization shall allow for development of new algorithms that go beyond what is possible with currently available datasets.
 
-Sample image template:
-<img src="https://github.com/dataset-ninja/gland-segmentation/assets/78355358/f158d0dd-71d5-41a2-aba5-4a5f57d54c35" alt="image" width="800">
+## Motivation
 
-<span style="font-size: smaller; font-style: italic;">Image description.</span>
+Pedestrian detection is a crucial problem within computer vision research, representing a specialized subset of object detection. However, the task's complexity arises from the diverse appearances of individuals and the critical need for high accuracy, particularly in applications like autonomous vehicle navigation. Consequently, pedestrian detection has evolved into a distinct field, garnering extensive research attention and spawning various algorithms with broad applications beyond their original scope.
+
+Despite significant advancements, particularly with the integration of deep learning methods, current monocular detectors still face limitations, particularly in heavily occluded scenarios. This constraint is understandable given the inherent ambiguity in identifying individuals under such conditions, solely relying on single-camera observations. Genuinely, multi-camera detectors come at hand. Important research body in the past decade has also been devoted on this topic. In general, simple averaging of the per-view predictions, can only improve upon a single view detector. Further, more sophisticated methods jointly make use of the information to yield a prediction.
+
+## Dataset description
+
--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,16 @@
-import supervisely as sly
+import glob
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+from urllib.parse import unquote, urlparse
 
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import file_exists, get_file_name, get_file_name_with_ext
+from supervisely.io.json import load_json_file
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +33,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +61,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +70,108 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    images_path = "/home/alex/DATASETS/TODO/archive/Wildtrack/Image_subsets"
+    bboxes_path = "/home/alex/DATASETS/TODO/archive/Wildtrack/annotations_positions"
+    batch_size = 30
+    bboxes_ext = ".json"
+    ds_name = "ds"
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    def create_ann(image_path):
+        labels = []
 
-    # ... some code here ...
+        subfolder_value = image_path.split("/")[-2]
+        camera_meta = folder_to_camera[subfolder_value]
+        camera = sly.Tag(camera_meta)
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        view_num = int(subfolder_value[1]) - 1
 
-    # return project
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
+        ann_path = os.path.join(bboxes_path, get_file_name(image_path) + bboxes_ext)
+        if file_exists(ann_path):
+            ann_data = load_json_file(ann_path)
+            for curr_ann_data in ann_data:
+                person_value = curr_ann_data["personID"]
+                person = sly.Tag(tag_person_id, value=person_value)
+                position_value = curr_ann_data["positionID"]
+                position = sly.Tag(tag_position_id, value=position_value)
 
+                for view in curr_ann_data["views"]:
+                    if view["viewNum"] == view_num:
+                        left = int(view["xmin"])
+                        top = int(view["ymin"])
+                        right = int(view["xmax"])
+                        bottom = int(view["ymax"])
+                        rect = sly.Rectangle(left=left, top=top, right=right, bottom=bottom)
+                        label = sly.Label(rect, obj_class, tags=[person, position])
+                        labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=[camera])
+
+    obj_class = sly.ObjClass("pedestrian", sly.Rectangle)
+    tag_c1 = sly.TagMeta("camera 1", sly.TagValueType.NONE)
+    tag_c2 = sly.TagMeta("camera 2", sly.TagValueType.NONE)
+    tag_c3 = sly.TagMeta("camera 3", sly.TagValueType.NONE)
+    tag_c4 = sly.TagMeta("camera 4", sly.TagValueType.NONE)
+    tag_c5 = sly.TagMeta("camera 5", sly.TagValueType.NONE)
+    tag_c6 = sly.TagMeta("camera 6", sly.TagValueType.NONE)
+    tag_c7 = sly.TagMeta("camera 7", sly.TagValueType.NONE)
+    tag_person_id = sly.TagMeta("person id", sly.TagValueType.ANY_NUMBER)
+    tag_position_id = sly.TagMeta("position id", sly.TagValueType.ANY_NUMBER)
+
+    folder_to_camera = {
+        "C1": tag_c1,
+        "C2": tag_c2,
+        "C3": tag_c3,
+        "C4": tag_c4,
+        "C5": tag_c5,
+        "C6": tag_c6,
+        "C7": tag_c7,
+    }
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(
+        obj_classes=[obj_class],
+        tag_metas=[
+            tag_c1,
+            tag_person_id,
+            tag_position_id,
+            tag_c2,
+            tag_c3,
+            tag_c4,
+            tag_c5,
+            tag_c6,
+            tag_c7,
+        ],
+    )
+    api.project.update_meta(project.id, meta.to_json())
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    images_pathes = glob.glob(images_path + "/*/*.png")
+
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
+
+    for img_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+        img_names_batch = [
+            im_path.split("/")[-2] + "_" + get_file_name_with_ext(im_path)
+            for im_path in img_pathes_batch
+        ]
+
+        img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_pathes_batch)
+        img_ids = [im_info.id for im_info in img_infos]
+
+        anns = [create_ann(image_path) for image_path in img_pathes_batch]
+        api.annotation.upload_anns(img_ids, anns)
+
+        progress.iters_done_report(len(img_names_batch))
+
+    return project
